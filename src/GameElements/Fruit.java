42efb878f5a223f66d17902acba1086755e78102
naomital <naomilau699@gmail.com>
2018-12-10 18:15:49 +0200

    init for class Fruit and Pacman

--- src/GameElements/Fruit.java
+++ src/GameElements/Fruit.java
@@ -1,10 +1,48 @@
 package GameElements;
 
-public class Fruit {
+import java.awt.Color;
 
-	public static void main(String[] args) {
+import GIS.Meta_data;
+import Geom.Point3D;
+
+public class Fruit implements Meta_data {
+
+	private Point3D point;
+	private String time;
+	private long UTC;
+	private Color color;
+
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getName() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setName(int name) {
+		// TODO Auto-generated method stub
+		
+	}
+public static void main(String[] args) {
 		
 		
 	}
-
 }
--- src/GameElements/Pacman.java
+++ src/GameElements/Pacman.java
@@ -1,7 +1,73 @@
 package GameElements;
 
-public class Pacman {
+import java.awt.Color;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+import GIS.Meta_data;
+import Geom.Point3D;
+
+public class Pacman implements Meta_data {
+	private Point3D point;
+	private String time;
+	private long UTC;
+	private Color color;
+	private String name;
+	private int speed;
+	private int radiusEat;
+	static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	public Pacman(Point3D point,String name,String time, Color color,int speed,int radiusEat ) throws ParseException{
+		this.point=point;
+		this.name=name;
+		this.color=color;
+		this.speed=speed;
+		this.radiusEat=radiusEat;
+		Date UTC = GMTtoUTC(time, -2);// this time fits to Israel summer time. 
+		Date oldDate = sdf.parse(time);
+		this.time= sdf.format(UTC);
+		this.UTC = UTC.getTime();//gets the client's first seen long type date in UTC time.
+	}
+	
+	public Date GMTtoUTC(String dateTime, int hours) throws ParseException {
+		Date oldDate = sdf.parse(dateTime);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(oldDate);
+		calendar.add(Calendar.HOUR_OF_DAY, hours);
+		return calendar.getTime();
+	}
+
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getName() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setName(int name) {
+		// TODO Auto-generated method stub
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
